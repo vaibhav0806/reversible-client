ab9121acfbbb5f3395dbfeabaac2d7abbb42bdd9
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2024-12-07 22:27:16 +0530

    remove local storage

--- app/components/navbar-next.tsx
+++ app/components/navbar-next.tsx
@@ -263,6 +263,7 @@ export function NavbarN() {
 
   const handleLogout = () => {
     localStorage.removeItem("userData");
+    localStorage.removeItem("walletData");
     setUserData(null);
     googleLogout();
   };
